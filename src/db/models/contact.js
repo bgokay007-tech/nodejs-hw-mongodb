14ee27a7c85e2ef320674087a48a8b9ba366e700
Burak <b.gokay007@gmail.com>
2026-05-30 03:45:58 +0300

    hw4-validation: add pagination, sorting, filtering and validation

--- src/db/models/contact.js
+++ src/db/models/contact.js
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
 
 const contactSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    phoneNumber: { type: String, required: true },
-    email: { type: String },
+    name: { type: String, required: true, minlength: 3, maxlength: 20 },
+    phoneNumber: { type: String, required: true, minlength: 3, maxlength: 20 },
+    email: { type: String, minlength: 3, maxlength: 20 },
     isFavourite: { type: Boolean, default: false },
     contactType: { type: String, enum: ['work', 'home', 'personal'], required: true, default: 'personal' },
 }, { timestamps: true });
